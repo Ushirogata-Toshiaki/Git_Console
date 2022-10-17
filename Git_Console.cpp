17b1a2986804540ca77be882e5e19763430afa22
Ushirogata-Toshiaki <114214733+Ushirogata-Toshiaki@users.noreply.github.com>
2022-10-17 16:36:49 +0900

    2行目のcoutを変更

--- Git_Console.cpp
+++ Git_Console.cpp
@@ -8,7 +8,7 @@ int main()
     std::cout << "Hello GitHub!\n";
 
 
-    std::cout << "Hello NewBranch!\n";
+    std::cout << "Hello NewBranchChanged!\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
