f32e1d130d63282824bf04798dd873109a785aff
Ushirogata-Toshiaki <114214733+Ushirogata-Toshiaki@users.noreply.github.com>
2022-10-19 14:50:21 +0900

    2行目をHello ConflictBranch!に変更

--- Git_Console.cpp
+++ Git_Console.cpp
@@ -6,7 +6,7 @@
 int main()
 {
     std::cout << "Hello GitHub World!\n";
-    std::cout << "Hello NewBranch!\n";
+    std::cout << "Hello ConflictBranch!\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
