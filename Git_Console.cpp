d4fa3f3903f4abbc77cdf51cf2e2a0e53e35ae0c
Ushirogata-Toshiaki <114214733+Ushirogata-Toshiaki@users.noreply.github.com>
2022-10-19 10:59:16 +0900

    Hello GitHub

--- Git_Console.cpp
+++ Git_Console.cpp
@@ -6,7 +6,7 @@
 int main()
 {
     std::cout << "Hello GitHub!\n";
-    std::cout << "Hello NewBranchChanged!\n";
+    std::cout << "Hello GitHub!\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
